5a13226e45eb1ddf40db3df32f500e3346c4b969
Niklas Furberg <niklas.furberg@gmail.com>
2020-08-04 00:15:52 +0200

    Plots sigmoid fn in test

--- test_nnmath.py
+++ test_nnmath.py
@@ -1,6 +1,7 @@
 import numpy as np
-import nnmath as m
 from numpy.testing import assert_allclose
+import matplotlib.pyplot as plt
+import nnmath as m
 
 def test_sigmoid_scalar():
     assert m.sigmoid(5) == 0.9933071490757153
@@ -14,3 +15,10 @@ def test_sigmoid_matrix():
     assert_allclose( \
         m.sigmoid(np.array([[0.001, 0.002], [0.002, 0.003]])), \
         np.array([[0.50025, 0.50050], [0.50050, 0.50075]]))
+
+def test_sigmoid_plot():
+    x = np.linspace(-10, 10, 1000)
+    plt.show( \
+        plt.plot( \
+            x, \
+            m.sigmoid(x)))
