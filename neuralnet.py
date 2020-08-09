49a2322c588da07d59fe10d4a34e03f1be08e9d0
Niklas Furberg <niklas.furberg@gmail.com>
2020-08-10 00:07:46 +0200

    Renames hypothesis to activate and also returns the sigmoid input

--- neuralnet.py
+++ neuralnet.py
@@ -1,6 +1,11 @@
+from collections import namedtuple
 import nnmath as m
 import numpy as np
 
 
-def hypothesis(x, theta):
-    return m.sigmoid(x @ np.transpose(theta))
+def activate(x, theta):
+    activation = namedtuple("activation", ["input", "output"])
+    input = x @ np.transpose(theta)
+    return activation(
+        input=input,
+        output=m.sigmoid(input))
--- test_neuralnet.py
+++ test_neuralnet.py
@@ -1,16 +1,28 @@
 import numpy as np
 import neuralnet as nn
 
-def test_hypothesis_matrix():
+
+def test_activate_matrix_correct_output():
     np.testing.\
-        assert_array_almost_equal( \
-            x=nn.hypothesis(\
-                x=np.array(\
-                    [[0.1, 0.2, 0.3],\
-                    [0.4, 0.5, 0.6]]),\
-                theta=np.array(\
-                    [[0.7, 0.8, 0.9],\
-                    [0.1, 0.11, 0.12]])), \
-            y=np.array(\
-                [[0.622459, 0.516993],\
-                [0.772064, 0.541653]]))
+        assert_array_almost_equal( 
+            x=nn.activate(
+                x=np.array(
+                    [[0.1, 0.2, 0.3], [0.4, 0.5, 0.6]]),
+                theta=np.array(
+                    [[0.7, 0.8, 0.9], [0.1, 0.11, 0.12]])
+            ).output,
+            y=np.array(
+                [[0.622459, 0.516993], [0.772064, 0.541653]]))
+
+
+def test_activate_matrix_correct_input():
+    np.testing.\
+        assert_array_almost_equal( 
+            x=nn.activate(
+                x=np.array(
+                    [[0.1, 0.2, 0.3], [0.4, 0.5, 0.6]]),
+                theta=np.array(
+                    [[0.7, 0.8, 0.9], [0.1, 0.11, 0.12]])
+            ).input,
+            y=np.array(
+                [[0.5, 0.068], [1.22, 0.167]]))
