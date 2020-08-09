54525653558dbf14c98e2bb599778abc647fe354
Niklas Furberg <niklas.furberg@gmail.com>
2020-08-10 00:38:14 +0200

    Improves param name

--- neuralnet.py
+++ neuralnet.py
@@ -11,5 +11,5 @@ def activate(x, theta):
         output=m.sigmoid(input))
 
 
-def add_bias_units(A):
-    return np.c_[np.ones(A.shape[0]), A]
+def add_bias_units(matrix):
+    return np.c_[np.ones(A.shape[0]), matrix]
