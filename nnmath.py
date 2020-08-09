65ff0db5b4f16330b368bd82a32e5902c49bc9c2
Niklas Furberg <niklas.furberg@gmail.com>
2020-08-10 00:10:27 +0200

    Fixes pep 8 compliance

--- nnmath.py
+++ nnmath.py
@@ -5,5 +5,5 @@ def sigmoid(z):
     return 1.0 / (1.0+np.exp(-z))
 
 
-def sigmoidgrad(z):
+def sigmoid_grad(z):
     return sigmoid(z) * (1-sigmoid(z))
--- test_nnmath.py
+++ test_nnmath.py
@@ -3,25 +3,29 @@ from numpy.testing import assert_allclose
 import matplotlib.pyplot as plt
 import nnmath as m
 
+
 def test_sigmoid_scalar():
     assert m.sigmoid(5) == 0.9933071490757153
 
+
 def test_sigmoid_vector():
-    assert_allclose( \
-        m.sigmoid(np.array([0.001, 0.002])), \
-        np.array([0.50025, 0.50050]))
+    assert_allclose(
+        actual=m.sigmoid(np.array([0.001, 0.002])),
+        desired=np.array([0.50025, 0.50050]))
+
 
 def test_sigmoid_matrix():
-    assert_allclose( \
-        m.sigmoid(np.array([[0.001, 0.002], [0.002, 0.003]])), \
-        np.array([[0.50025, 0.50050], [0.50050, 0.50075]]))
+    assert_allclose(
+        actual=m.sigmoid(np.array([[0.001, 0.002], [0.002, 0.003]])),
+        desired=np.array([[0.50025, 0.50050], [0.50050, 0.50075]]))
+
 
 def test_nnmath_plot():
     x = np.linspace(-10, 10, 100)
-    plt.plot( \
-        x, \
-        m.sigmoidgrad(x))    
-    plt.plot( \
-        x, \
+    plt.plot(
+        x,
+        m.sigmoid_grad(x))
+    plt.plot(
+        x,
         m.sigmoid(x))
     plt.show()
