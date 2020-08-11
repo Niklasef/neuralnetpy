c97600354e0f950007375b4738a2d43d37de3654
Niklas Furberg <niklas.furberg@gmail.com>
2020-08-12 00:00:13 +0200

    Adds cost

--- neuralnet.py
+++ neuralnet.py
@@ -6,6 +6,28 @@ from sklearn.preprocessing import OneHotEncoder
 layer = namedtuple("layer", ["input", "output", "weight"])
 
 
+def cost(layer_one, layer_two_weight, layer_three_weight, y):
+    layer_two = activate(
+        layer_one,
+        layer_two_weight)
+    layer_three = activate(
+        layer_two,
+        layer_three_weight)
+    return J(
+        h=layer_three.output,
+        y=OneHotEncoder(sparse=False).fit_transform(y),
+        m=layer_one.output.shape[0])
+
+
+def J(h, y, m):
+    return \
+        (np.sum(
+            np.sum(
+                (-y * np.log(h))
+                - ((1-y) * (np.log(1-h)))))) \
+        / m
+
+
 def activate(precedent_layer, weight):
     input = add_bias_units(precedent_layer.output) @ np.transpose(weight)
     return layer(
